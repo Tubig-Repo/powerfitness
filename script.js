d939de239c033e394acb742e78e996c76e05763d
Robert Carlo <carlotubig@gmail.com>
2022-07-08 17:06:15 +0900

    added slider

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//Header variables
 const menu = document.querySelector(".menu_container");
 const links = document.querySelector(".links__container");
 const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
+// Slider Variables
+const slides = document.querySelectorAll(".slide");
+const dotContainer = document.querySelector(".dots");
 // Navigation Slider
 let navExpanded = false;
 menu.addEventListener("click", function (e) {
@@ -25,3 +29,53 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+//Sliders and Dot Function
+let curSlide = 0;
+let maxSlide = slides.length;
+
+//Creating dot Buttons
+
+// Dynamically Creating dots depending on the number of elements
+const createDots = function () {
+  slides.forEach((_, index) => {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${index}"></button>`
+    );
+  });
+};
+
+//Sliding function
+const goToSlide = function (slide) {
+  slides.forEach((el, index) => {
+    el.style.transform = `translateX(${100 * (index - slide)}%)`;
+  });
+};
+// Dot Navigation function
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
+
+// dots active styling function
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((t) => t.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add(`dots__dot--active`);
+};
+
+const init = function () {
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+};
+
+init();
